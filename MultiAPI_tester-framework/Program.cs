c818d0c56d0448a53dd1e8ad40333629bbdb8179
Plufik <dmitry090209@gmail.com>
2024-02-26 00:32:59 +0300

    Создано событие сборки для объединения всех библиотек в одну

--- MultiAPI_tester-framework/Program.cs
+++ MultiAPI_tester-framework/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using MultiAPI;
-using Microsoft.Win32;
 
 namespace MultiAPI_Tester
 {

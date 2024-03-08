f4a47d4883e053a14e87bf53598e3d4de0196b88
Plufik <dmitry090209@gmail.com>
2024-03-08 23:38:55 +0300

    Update 0.1.2.93 [NOT STABLE] + Добавлен новый класс: WinAPI и WinAPI.Window

--- MultiAPI_tester-framework/Program.cs
+++ MultiAPI_tester-framework/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using MultiAPI;
 
 namespace MultiAPI_Tester
 {
@@ -8,10 +9,6 @@ namespace MultiAPI_Tester
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("E:\\repos\\MultiAPI\\MultiAPI_lib-framework\\Main.cs");
-            string line24 = lines[23]; // индексация начинается с 0, поэтому 24-я строка имеет индекс 23
-            string textInBrackets = line24.Split('"')[1]; // извлечение текста в скобках
-            Console.WriteLine(textInBrackets);
             Console.ReadLine();
         }
 

8828360c412488a28d28af38e8ec45b34d1b039b
Vijaye Raji <vijaye@statsig.com>
2025-03-06 20:21:01 +0300

    A SUPER MEGA FIX THAT I'VE ONLY FUCKING REALIZED NOW.
    - Fixed internal library conflicts
    
    [0.1.5.224]

--- MultiAPI_lib-framework/Main.cs
+++ MultiAPI_lib-framework/Main.cs
@@ -7,7 +7,7 @@ using System.Threading;
  * Class:        | Basic
  * Project:      | MultiAPI
  * Author:       | dmitriykotik
- * Version:      | 0.1.4.221
+ * Version:      | 0.1.5.224
  * VerType:      | major_version.minor_version.patch_version.build
  * Main file:    | Main.cs
  * [OPEN SOURCE] | +True
@@ -20,7 +20,7 @@ namespace MultiAPI
     #region Assembly Info
     internal class AssemblyI
     {
-        internal const string version = "0.1.4.221";
+        internal const string version = "0.1.5.224";
     }
     #endregion
 
